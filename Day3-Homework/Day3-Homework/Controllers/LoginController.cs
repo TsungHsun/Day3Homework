a423b76c5748b9be7aab7565633e22d5cee8d86a
TsungHsun <fb282@yahoo.com.tw>
2016-11-03 18:47:14 +0800

    建立登入頁面,測試變綠燈

---
+++ Day3-Homework/Day3-Homework/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day3_Homework.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string account, string password)
+        {
+            if (account == "joeychen" && password == "1234")
+            {
+                return RedirectToAction("Index", "Welcome");
+            }
+
+            return View();
+        }
+    }
+}
